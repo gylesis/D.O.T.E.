6af2fae6bca4442d60fd90a3c86b991e0d9a9266
Dan <danilka2315.g@gmail.com>
2020-12-12 21:16:53 +0600

    asdCommit
    asdEcription

---
+++ Assets/asdScript.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asdScript : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=OLpeX4RRo28&ab_channel=TVFilthyFrank
+}
